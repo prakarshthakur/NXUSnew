4c352219408b0acc6da5b473159508a0918cae08
Prakarsh Thakur <prakarshyo@gmail.com>
2026-04-02 14:22:41 +0530

    fix(sw): neutralize aggressive cache

--- sw.js
+++ sw.js
@@ -1,33 +1,13 @@
-const CACHE_NAME = 'nxus-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/analytics.js',
-  '/firebase-auth.js',
-  '/host.html',
-  '/likes.html',
-  '/login.html',
-  '/settings.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
   );
 });
