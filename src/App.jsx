ffa367147f67c4bc70ad444d9012a536e6413326
deusabove <prakarshyo@gmail.com>
2026-04-07 20:05:35 +0530

    auto: update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './hooks/useAuth';
 import Login from './pages/Login';
